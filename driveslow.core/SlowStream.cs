24ca3f579b31a9df4910f261d480f24a75345e9b
Tim Preston <tim.preston@uitlanders.com>
2014-09-29 21:20:18 +1000

    Init slowstream + testing
    Stream implementation that allows rate limit

---
+++ driveslow.core/SlowStream.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace driveslow.core
+{
+    /// <summary>
+    /// This class provides a slower wrapper around an underlying stream.
+    /// </summary>
+    public class SlowStream : Stream
+    {
+        private Stream baseStream;
+        private Stopwatch swTimer;
+        /// <summary>
+        /// Time slice to use  (ms)
+        /// </summary>
+        private const int TimeSlice=100;
+        private readonly int rate;
+        private readonly int bytesPerTimeSlice;
+
+        private int totalBytesRead = 0;
+
+        public SlowStream(Stream baseStreamToUse, int kbPerSecond)
+        {
+            if (baseStreamToUse == null) throw new ArgumentNullException("baseStreamToUse");
+            if(kbPerSecond == 0) throw new ArgumentException("Requires a value > 0. (kB/sec)", "kbPerSecond");
+            baseStream = baseStreamToUse;
+            rate = kbPerSecond*1024;
+            bytesPerTimeSlice = rate/(1000/TimeSlice);
+            swTimer = new Stopwatch();
+            swTimer.Start();
+        }
+
+        public new void Dispose()
+        {
+            swTimer.Stop();
+            baseStream.Dispose();
+            base.Dispose();
+        }
+
+        public override void Flush()
+        {
+            baseStream.Flush();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return baseStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            baseStream.SetLength(value);
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var startOfSlice = swTimer.ElapsedMilliseconds;
+            var maxCountToRead = Math.Min(bytesPerTimeSlice, count);
+            var bytesRead = baseStream.Read(buffer, offset, maxCountToRead);
+            totalBytesRead += bytesRead;
+            var difference = swTimer.ElapsedMilliseconds - startOfSlice;
+            var totalElapsed = swTimer.ElapsedMilliseconds;
+            if (totalElapsed == 0)
+            {
+                totalElapsed = 1;
+            }
+            var currentRate = totalBytesRead*1.0/totalElapsed*1000;
+            bool isBelowDesiredRate = currentRate < rate*0.90;
+            bool isAboveDesiredRate = currentRate > rate*1.10;
+            if (isAboveDesiredRate)
+            {
+                //Sleep for partial timeslice if going to fast.
+                Thread.Sleep(TimeSlice/4);
+            }
+
+            if (isBelowDesiredRate)
+            {
+                //Dont sleep if below current rate.
+            }
+            else if (difference == 0)
+            {
+                //Sleep for timeslice.
+                Thread.Sleep(TimeSlice);
+            }
+            else if (difference > 0 && difference < TimeSlice)
+            {
+                //Sleep remainder of timeslice.
+                Thread.Sleep(TimeSlice - (int) difference);
+            }
+            //Else already taken too long
+            return bytesRead;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            baseStream.Write(buffer, offset, count);
+        }
+
+        public override bool CanRead
+        {
+            get { return baseStream.CanRead; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return baseStream.CanSeek; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return baseStream.CanWrite; }
+        }
+
+        public override long Length
+        {
+            get { return baseStream.Length; }
+        }
+
+        public override long Position
+        {
+            get { return baseStream.Position; }
+            set { baseStream.Position = value; }
+        }
+    }
+}
---
+++ driveslowtesing/SlowStreamTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using driveslow.core;
+using NUnit.Framework;
+
+namespace driveslowtesting
+{
+    [TestFixture]
+    public class SlowStreamTests
+    {
+
+        private readonly string Temp100kFile = "Temp.100k.tmp";
+        private readonly string Temp1MBFile = "Temp.1MB.tmp";
+        private Stopwatch timing = new Stopwatch();
+        [SetUp]
+        public void SetUp()
+        {
+            using (var new100kFile = new FileStream(Temp100kFile, FileMode.Create))
+            {
+                new100kFile.Write(new byte[100*1024], 0, 100*1024);
+            }
+            using (var new1MBFile = new FileStream(Temp1MBFile, FileMode.Create))
+            {
+                new1MBFile.Write(new byte[10*100*1024], 0, 10*100*1024);
+            }
+            timing = new Stopwatch();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            timing = null;
+        }
+
+        private void PerformTestAtRate(int rateInKBs, string tempFileToUse)
+        {
+            timing.Restart();
+            int bytesRead = 0;
+            using (var file = new SlowStream(new FileStream(tempFileToUse, FileMode.Open, FileAccess.Read), rateInKBs))
+            {
+                var buffer = new byte[4096];
+                do
+                {
+                    bytesRead += file.Read(buffer, 0, buffer.Length);
+                } while (file.CanRead && file.Position != file.Length && bytesRead < file.Length);
+            }
+            var time = timing.ElapsedMilliseconds;
+            var kbBytes = bytesRead/1024.0;
+            var msTime = time/1000.0;
+            System.Console.WriteLine("{0}kb in {1:0.00}s == {2:0.00} kb/s", kbBytes, msTime, kbBytes/msTime);
+            Thread.Sleep(1000);
+        }
+
+        [Test]
+        public void InitialReadTest_1MB()
+        {
+            PerformTestAtRate(5, Temp1MBFile);
+        }
+
+        [Test]
+        public void InitialReadTest_100k()
+        {
+            PerformTestAtRate(5, Temp100kFile);
+        }
+
+        [Test, Explicit]
+        public void TestMultipleRates()
+        {
+            var ratesToTest = new[]
+            {
+                100,
+                80,
+                50,
+                20,
+                15,
+                10,
+                5,
+                2,
+            }.ToList();
+            ratesToTest.ForEach(zz =>
+            {
+                Console.WriteLine("Perform test at : {0} kb/s", zz);
+                PerformTestAtRate(zz, Temp1MBFile);
+            });
+        }
+    }
+}
